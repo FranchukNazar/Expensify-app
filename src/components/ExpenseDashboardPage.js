3d4d86ce4a58fd44e6b23211b793c20374d87223
Nazar <nazfranuk@gmail.com>
2018-06-07 20:19:18 +0300

    add Redux

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import ExpenseList from './ExpenseList'
 
 const ExpenseDashboardPage = () => (
     <div>
-        dashboard component
+        <ExpenseList />
     </div>
 );
 

cb808367bc1b0cb9a56491f1fc6213cc4b128658
Nazar <nazfranuk@gmail.com>
2018-06-08 21:56:06 +0300

    add Redux to the app

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { AddExpense, addExpense } from '../actions/expenses';
 
-const AddExpensePage = () => (
+
+const AddExpensePage = (props) => (
     <div>
-        add component
+        <h1>Add expens</h1>
+        <ExpenseForm
+            onSubmit={(expense) => {
+                props.dispatch(addExpense(expense));
+                props.history.push('/');
+            }}
+        />
     </div>
 );
 
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
